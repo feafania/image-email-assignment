6ae8badd82e430affde0bd0a93d9032a7c0a7df8
feafania <173483105+feafania@users.noreply.github.com>
2026-09-05 16:47:27 +0100

    feat: add error store

---
+++ js/config/errors.js
@@ -0,0 +1,8 @@
+const ERROR_MESSAGES = {
+  IMAGE_NOT_FOUND: 'Image not found',
+  IMAGE_ALREADY_EXISTS: 'Image already exists',
+  INVALID_EMAIL: 'Invalid email',
+  IMAGE_UPLOAD_FAILED: 'Failed to upload image',
+};
+
+export default ERROR_MESSAGES;
